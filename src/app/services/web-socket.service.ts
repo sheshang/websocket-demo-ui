723cc2ebb20dcaeb9b2d45a374dfdddb3d1e3d2e
sheshang <sidajwalia@gmail.com>
2021-09-18 15:49:15 +0530

    retry logic

--- src/app/services/web-socket.service.ts
+++ src/app/services/web-socket.service.ts
@@ -12,20 +12,30 @@ export class WebSocketService {
   constructor() { }
 
   public openWebSocket(){
-    this.webSocket = new WebSocket('ws://localhost:8080/chat');
+    if (!this.webSocket || this.webSocket.CLOSED) {
+      this.webSocket = new WebSocket('ws://localhost:8080/chat');
 
-    this.webSocket.onopen = (event) => {
-      console.log('Open: ', event);
-    };
-
-    this.webSocket.onmessage = (event) => {
-      const chatMessageDto = JSON.parse(event.data);
-      this.chatMessages.push(chatMessageDto);
-    };
-
-    this.webSocket.onclose = (event) => {
-      console.log('Close: ', event);
-    };
+      this.webSocket.onopen = (event) => {
+        console.log('Open: ', event);
+        clearTimeout();
+      };
+  
+      this.webSocket.onmessage = (event) => {
+        const chatMessageDto = JSON.parse(event.data);
+        this.chatMessages.push(chatMessageDto);
+      };
+  
+      this.webSocket.onerror = (event) => {
+        console.log('Error: ', event);
+        this.webSocket.close();
+      };
+  
+      this.webSocket.onclose = (event) => {
+        console.log('Close: ', event);
+        console.log('Retry: ', event);
+        this.openWebSocket();
+      };
+    }
   }
 
   public sendMessage(chatMessageDto: ChatMessageDto){
